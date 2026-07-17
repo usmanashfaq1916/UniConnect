9fc969d47800eb315bce7a1cdff2f5925a99f02d
usmanashfaq1916 <usman.ashfaq1916@gmail.com>
2026-07-17 15:09:02 +0500

    Initial commit: UniConnect - university admission portal

---
+++ src/lib/db.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+function createPrismaClient() {
+  const url = process.env.DATABASE_URL!;
+  const adapter = new PrismaPg(url);
+  return new PrismaClient({ adapter });
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
